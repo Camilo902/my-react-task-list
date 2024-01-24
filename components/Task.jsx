35b6960dbdd56b8953597d103d41be92c3c254f7
Camilo902 <Camilo902@users.noreply.github.com>
2024-01-24 16:25:48 -0500

    Implementando Hooks y Css

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,10 +1,15 @@
-function Task({ name, isCompleted, onToggle }) {
-    return (
+import React from 'react';
+
+function Task({ title, description, isCompleted, onToggle }) {
+  return (
+    <div>
       <div>
-        <span style={{ textDecoration: isCompleted ? 'line-through' : 'none' }}>{name}</span>
+        <span style={{ textDecoration: isCompleted ? 'line-through' : 'none' }}>{title}</span>
         <input type="checkbox" checked={isCompleted} onChange={onToggle} />
       </div>
-    );
-  }
+      <p>{description}</p>
+    </div>
+  );
+}
 
-export {Task};
+export default Task;
--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
-import { Task } from "./Task";
+// TaskList.jsx
+
+import React, { useState, useEffect } from "react";
+import Task from "./Task";
+import "./TaskList.css";
 
 function TaskList() {
-  const [tasks, setTasks] = useState([
-    { id: 1, name: "Buy a new gaming laptop", isCompleted: false },
-    { id: 2, name: "Complete a previous task", isCompleted: false },
-    { id: 3, name: "Create video for youtube", isCompleted: false },
-    { id: 4, name: "Create a new portafolio site", isCompleted: false },
-  ]);
+  const [tasks, setTasks] = useState(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    return storedTasks ? JSON.parse(storedTasks) : [];
+  });
+
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+  const [editingTask, setEditingTask] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleTaskCompletion = (taskId) => {
     setTasks((prevTasks) =>
@@ -17,18 +26,127 @@ function TaskList() {
     );
   };
 
+  const addTask = () => {
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        id: prevTasks.length + 1,
+        title: newTaskTitle,
+        description: newTaskDescription,
+        isCompleted: false,
+      },
+    ]);
+
+    setNewTaskTitle("");
+    setNewTaskDescription("");
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
+  const startEditingTask = (taskId) => {
+    const taskToEdit = tasks.find((task) => task.id === taskId);
+    setEditingTask(taskToEdit);
+  };
+
+  const updateTask = () => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === editingTask.id
+          ? {
+              ...task,
+              title: editingTask.title,
+              description: editingTask.description,
+            }
+          : task
+      )
+    );
+    setEditingTask(null);
+  };
+
   return (
-    <ul>
-      {tasks.map((task) => (
-        <li key={task.id}>
-          <Task
-            name={task.name}
-            isCompleted={task.isCompleted}
-            onToggle={() => toggleTaskCompletion(task.id)}
-          />
-        </li>
-      ))}
-    </ul>
+    <div className="task-list-container">
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id} className="task">
+            {editingTask?.id === task.id ? (
+              <div className="task-edit-form">
+                <label className="label">Title:</label>
+                <input
+                  type="text"
+                  value={editingTask.title}
+                  onChange={(e) =>
+                    setEditingTask({ ...editingTask, title: e.target.value })
+                  }
+                  className="input-field"
+                />
+                <label className="label">Description:</label>
+                <input
+                  type="text"
+                  value={editingTask.description}
+                  onChange={(e) =>
+                    setEditingTask({
+                      ...editingTask,
+                      description: e.target.value,
+                    })
+                  }
+                  className="input-field"
+                />
+                <button onClick={updateTask} className="button">
+                  Update
+                </button>
+              </div>
+            ) : (
+              <>
+                <Task
+                  title={task.title}
+                  description={task.description}
+                  isCompleted={task.isCompleted}
+                  onToggle={() => toggleTaskCompletion(task.id)}
+                />
+                <div className="task-actions">
+                  <button
+                    onClick={() => deleteTask(task.id)}
+                    className="button"
+                  >
+                    Delete
+                  </button>
+                  <button
+                    onClick={() => startEditingTask(task.id)}
+                    className="button"
+                  >
+                    Edit
+                  </button>
+                </div>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <label className="label">Title:</label>
+        <input
+          type="text"
+          value={newTaskTitle}
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+          className="input-field"
+        />
+        <br />
+        <label className="label">Description:</label>
+        <input
+          type="text"
+          value={newTaskDescription}
+          onChange={(e) => setNewTaskDescription(e.target.value)}
+          className="input-field"
+        />
+        <br />
+        <button onClick={addTask} className="button">
+          Add Task
+        </button>
+      </div>
+    </div>
   );
 }
 
